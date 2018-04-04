c476cd1286a0e6c78863eeeef9b2eb16d22797e4
Kleber Almeida Toyota <kleber.toyota@concrete.com.br>
2018-04-04 16:55:21 -0300

    Inicial

--- src/main/java/com/alves/adriel/zuul/ZuulApplication.java
+++ src/main/java/com/alves/adriel/zuul/ZuulApplication.java
@@ -2,6 +2,8 @@ package com.alves.adriel.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
