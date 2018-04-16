51263b736d236645735897b2d8c726354ab6d335
julianoborba <juliano_borba@riseup.net>
2018-04-16 13:28:23 -0300

    Adciiona bucket4j

--- src/main/java/com/alves/adriel/zuul/ZuulApplication.java
+++ src/main/java/com/alves/adriel/zuul/ZuulApplication.java
@@ -2,12 +2,14 @@ package com.alves.adriel.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @EnableEurekaClient 
 @SpringBootApplication
+@EnableCaching
 public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
